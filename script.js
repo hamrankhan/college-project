6de5d2ac8bdc575bd3d0b4101e5fa6259226c065
MofiqueKhan <khanmofique@gmail.com>
2023-09-30 01:29:04 +0530

    formating

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log("welcome..!!!");
+const BFcontainer = document.getElementById("hiddenFB")
+const BBcontainer = document.getElementById("hiddenBB")
+const Acontainer = document.getElementById("AB")
+const Abtn = document.getElementById("Abtn")
+const Bbtn = document.getElementById("Bbtn")
 
+Abtn.addEventListener("click",()=>{
+    BFcontainer.style.visibility = "hidden";
+    BBcontainer.style.visibility = "hidden";
+    Acontainer.style.visibility = "visible";
+})
